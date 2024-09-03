acca51641cf8cbc947ac336f60af7167d84d2632
Juan D. Lora <juan.lora1@u.icesi.edu.co>
2024-09-03 12:24:46 -0500

    commentservices improved

--- src/schemas/comment.schema.ts
+++ src/schemas/comment.schema.ts
@@ -2,7 +2,8 @@ import {object, string, TypeOf } from 'zod'
 
 const createCommentSchema = object({
     content: string({ required_error: "Content is required" }),
-    parent: string(),  //
+    //user:,
+    parent: string(), 
 });
 
 //update and delete
